7b97119bed4607d5487ae4841bcd100f1f42de00
Xochitl Menjivar <xandra.menji@gmail.com>
2018-02-04 19:33:20 +0000

    automatically backed up by learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -7,12 +7,3 @@ describe "current_age_for_birth_year method" do
     expect(age_of_person).to eq(19)
   end
 end
-
-require_relative '../current_age_for_birth_year.rb'
- 
-puts "What year were you born?"
-birth_year = gets.to_i
- 
-users_age = current_age_for_birth_year(birth_year) + (Time.now.year - 2003)
- 
-puts "You are: " + users_age.to_s + " years old."
